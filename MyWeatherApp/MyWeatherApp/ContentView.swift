d2949bea36bdb3a3afc05487101ee926a7221abf
fyky <fuyu12dmb25ms@gmail.com>
2022-01-31 16:42:27 +0900

    [Add]WeatherApp

---
+++ MyWeatherApp/MyWeatherApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MyWeatherApp
+//
+//  Created by fyky on 2022/01/31.
+//
+
+import SwiftUI
+import SwiftyJSON
+import Alamofire
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Swift!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyWeatherApp/MyWeatherApp/MyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherAppApp.swift
+//  MyWeatherApp
+//
+//  Created by fyky on 2022/01/31.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
